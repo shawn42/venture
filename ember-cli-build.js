271586b01988bf83a1a4ad6fda166a6f11fb9ddd
shawn42 <shawn42@gmail.com>
2015-09-17 02:23:22 +0000

    post class code

--- ember-cli-build.js
+++ ember-cli-build.js
@@ -6,6 +6,7 @@ module.exports = function(defaults) {
     // Add options here
   });
 
+  app.import('bower_components/bootstrap-sass/assets/javascripts/bootstrap.js');
   // Use `app.import` to add additional libraries to the generated
   // output files.
   //
