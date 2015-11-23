12322f6f3a8755f5496bcd3cde9a26427209e414
Shawn Anderson <shawn42@gmail.com>
2015-11-23 02:41:49 -0500

    update character position and tiles

---
+++ app/adapters/application.js
@@ -0,0 +1,8 @@
+import DS from 'ember-data';
+import Ember from 'ember';
+
+export default DS.ActiveModelAdapter.extend({
+  // pathForType: function(type) {
+  //   return Ember.String.underscore(type);
+  // }
+});
